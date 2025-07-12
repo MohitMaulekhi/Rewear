849d42582e5ffa33026ecb693e658b09105c1db7
Dia-Jainn <dia.jainn.ug23@nsut.ac.in>
2025-07-12 15:36:57 +0530

    fonts and hovering cursor updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './global.css';
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 import { Toaster } from 'react-hot-toast'
